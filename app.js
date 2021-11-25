505017eae0c9bfefc8cdd25680c1f4b4c4077401
Muhammad Fahad <fahadsahib786@gmail.com>
2021-11-25 20:22:27 +0500

    FruitsProject

--- app.js
+++ app.js
@@ -3,56 +3,108 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/fruitsDB", {UseNewUrlParser: true});
 
 const fruitSchema = new mongoose.Schema({
-name: String,
-rating: Number,
+name: {
+    type: String,
+    required: [true, "Please enter the fruit name first."]
+},
+rating: {
+    type: Number,
+    min: 1,
+    max: 10
+},
 review: String
 });
 
 const Fruit = mongoose.model("Fruit",fruitSchema);
 
 const fruit = new Fruit ({
-    name: "Apple",
-    rating: 5,
-    review: "This was an having an amazing taste"
-});
 
-
-const personSchema = new mongoose.Schema({
-    name: String,
-    age: Number
-});
-
-const Person = mongoose.model("Person",personSchema);
-
-const person = new Person ({
-    name: "Muhammad Fahad",
-    age: 21
-});
-
-person.save();
-
-const kiwi = new Fruit ({
-    name: "Kiwi",
     rating: 10,
-    review: "amazing taste"
+    review: "Peach was an having an amazing taste"
 });
 
-const orange = new Fruit ({
-    name: "Orange",
-    rating: 7,
-    review: "Citrus fruit"
-});
+// fruit.save();
 
-const banana = new Fruit ({
-    name: "Banana",
-    rating: 6,
-    review: "Juicy"
-});
+// const personSchema = new mongoose.Schema({
+//     name: String,
+//     age: Number
+// });
 
-Fruit.insertMany([kiwi,orange,banana],function (err) {
-    if (err){
+// const Person = mongoose.model("Person",personSchema);
+
+// const person = new Person ({
+//     name: "Muhammad Fahad",
+//     age: 21
+// });
+
+// person.save();
+
+// const kiwi = new Fruit ({
+//     name: "Kiwi",
+//     rating: 10,
+//     review: "amazing taste"
+// });
+
+// const orange = new Fruit ({
+//     name: "Orange",
+//     rating: 7,
+//     review: "Citrus fruit"
+// });
+
+// const banana = new Fruit ({
+//     name: "Banana",
+//     rating: 6,
+//     review: "Juicy"
+// });
+
+
+//inserting many entries at once
+
+// Fruit.insertMany([kiwi,orange,banana],function (err) {
+//     if (err){
+//         console.log(err);
+//     }else {
+//         console.log("Successfully added all the fruits to fruitsDB!.");
+//     }
+// });
+
+
+
+//Searching/finding data entries from database
+
+Fruit.find(function (err,fruits) {
+    if (err) {
         console.log(err);
     }else {
-        console.log("Successfully added all the fruits to fruitsDB!.");
+        fruits.forEach(function (fruit) {
+        console.log(fruit.name);
+        });
+        mongoose.connection.close();
+
     }
 });
+
+
+//Updating Fruit Name
+
+
+// Fruit.updateOne({_id: "619f74c9dca409e8454a6eb8"},{name: "Peaches"},function (err) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Successfully added fruit!");
+//     }
+// });
+
+
+
+//Deleting fruit from database
+
+// Fruit.deleteOne({name:"Peaches"},function (err) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Successfully deleted!");
+//     }
+//     mongoose.connection.close();
+// });
